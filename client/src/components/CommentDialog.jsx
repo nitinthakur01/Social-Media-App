0e72892918f4458875cbc2fbfbd4c3fe5480f9bf
Nitin Thakur <nitinthakur46323@gmail.com>
2025-10-09 10:30:20 +0530

    comment dialog

--- client/src/components/CommentDialog.jsx
+++ client/src/components/CommentDialog.jsx
@@ -1,15 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import { Dialog, DialogContent } from "./ui/dialog";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Link } from "react-router-dom";
+import { DialogTrigger } from "@radix-ui/react-dialog";
+import { MoreHorizontal } from "lucide-react";
+import { Button } from "./ui/button";
 
 function CommentDialog({ open, setOpen }) {
+  const [text, setText] = useState("");
+
+  const changeEventHandler = (e) => {
+    const inputText = e.target.value;
+    if (inputText.trim()) {
+      setText(inputText);
+    } else {
+      setText("");
+    }
+  };
+
+  const sendMessageHandler = async () => {
+    alert(text);
+    setText("");
+  };
   return (
     <Dialog open={open}>
-      <DialogContent onInteractOutside={() => setOpen(false)}>
-        <img
-          className="rounded-sm my-2 w-full aspect-square object-cover"
-          src="https://plus.unsplash.com/premium_photo-1664121799890-b5605834b72a?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="Comment_img"
-        />
+      <DialogContent
+        onInteractOutside={() => setOpen(false)}
+        className="!max-w-4xl p-0 flex flex-col"
+      >
+        <div className="flex flex-1">
+          <div className="w-1/2">
+            <img
+              src="https://plus.unsplash.com/premium_photo-1664121799890-b5605834b72a?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="comment_img"
+              className="w-full h-full object-cover rounded-l-lg"
+            />
+          </div>
+
+          <div className="w-1/2 flex flex-col justify-between">
+            <div className="flex items-center justify-between p-4">
+              <div className="flex gap-3 items-center">
+                <Link>
+                  <Avatar>
+                    <AvatarImage src="" alt="comment_img" />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                </Link>
+                <div>
+                  <Link className="font-semibold text-xs">username</Link>
+                  {/* <span className="text-gray-600 text-sm">Bio here...</span> */}
+                </div>
+              </div>
+              <Dialog>
+                <DialogTrigger>
+                  <MoreHorizontal className="cursor-pointer" />
+                </DialogTrigger>
+                <DialogContent className="flex flex-col items-center text-sm text-center">
+                  <div className="cursor-pointer w-full text-[#ED4956] font-bold">
+                    Unfollow
+                  </div>
+                  <div className="cursor-pointer w-full">Add to favorites</div>
+                </DialogContent>
+              </Dialog>
+            </div>
+            <hr />
+            <div className="flex-1 overflow-y-auto max-h-96 p-4">
+              Comment to be display here
+            </div>
+            <div className="p-4">
+              <div className="flex items-center gap-2">
+                <input
+                  type="text"
+                  value={text}
+                  onChange={changeEventHandler}
+                  placeholder="Add a comment...."
+                  className="w-full outline-none border text-sm border-gray-300 p-2 rounded "
+                />
+                <Button
+                  disabled={!text.trim()}
+                  onClick={sendMessageHandler}
+                  className="cursor-pointer"
+                  variant="outline"
+                >
+                  Send
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   );
